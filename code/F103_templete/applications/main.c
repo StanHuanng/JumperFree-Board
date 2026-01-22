2b2d96b4908e90c2d50729bbd0c96195a933ec6f
StanHuanng <Shilostan08@outlook.com>
2026-01-22 16:26:04 +0800

    修改空白模板，添加ws2812驱动

--- code/F103_templete/applications/main.c
+++ code/F103_templete/applications/main.c
@@ -4,25 +4,10 @@
 
 int main(void)
 {
-    /* 初始化 CH446Q 驱动 */
-    ch446q_init();
-
-	ch446q_reset_all();
-	
-    /* 示例：将 X0 连接到 Y0 */
-    ch446q_connect(0, 0);
-    rt_kprintf("Connected X0 to Y0\n");
-    rt_thread_mdelay(1000);
-
-    /* 示例：将 X3 连接到 Y2 */
-    ch446q_connect(3, 2);
-    rt_kprintf("Connected X3 to Y2\n");
-    rt_thread_mdelay(1000);
-
-
+    rt_kprintf("Hello Rt_Thread\n");
 
     while (1)
     {
-        rt_thread_mdelay(1000);
+
     }
 }
